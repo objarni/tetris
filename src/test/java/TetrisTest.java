23abe2f1f39673fd3178e28007240fddf0428732
Olof Bjarnason <olof.bjarnason@gmail.com>
2023-01-15 13:03:17 +0100

    add JUnit5 and a true==true test

---
+++ src/test/java/TetrisTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class TetrisTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void upPressed() {
+        assertTrue(true);
+    }
+}
