daa39129b682d6ccfa1d351da8a0c0c3e6a068ca
Erik Kinding <erikkinding@gmail.com>
2023-02-03 10:36:26 +0100

    cleaning up comments in Location.java

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,80 +1,24 @@
-/**
- * A <code>source.Location</code> object represents the row and column of a location
- * in a two-dimensional grid. <br />
- * The API of this class is testable on the AP CS A and AB exams.
- */
+
 public class Location implements Comparable
 {
     private int row; // row location in grid
     private int col; // column location in grid
 
-    /**
-     * The turn angle for turning 90 degrees to the left.
-     */
     public static final int LEFT = -90;
-    /**
-     * The turn angle for turning 90 degrees to the right.
-     */
     public static final int RIGHT = 90;
-    /**
-     * The turn angle for turning 45 degrees to the left.
-     */
     public static final int HALF_LEFT = -45;
-    /**
-     * The turn angle for turning 45 degrees to the right.
-     */
     public static final int HALF_RIGHT = 45;
-    /**
-     * The turn angle for turning a full circle.
-     */
     public static final int FULL_CIRCLE = 360;
-    /**
-     * The turn angle for turning a half circle.
-     */
     public static final int HALF_CIRCLE = 180;
-    /**
-     * The turn angle for making no turn.
-     */
     public static final int AHEAD = 0;
-
-    /**
-     * The compass direction for north.
-     */
     public static final int NORTH = 0;
-    /**
-     * The compass direction for northeast.
-     */
     public static final int NORTHEAST = 45;
-    /**
-     * The compass direction for east.
-     */
     public static final int EAST = 90;
-    /**
-     * The compass direction for southeast.
-     */
     public static final int SOUTHEAST = 135;
-    /**
-     * The compass direction for south.
-     */
     public static final int SOUTH = 180;
-    /**
-     * The compass direction for southwest.
-     */
     public static final int SOUTHWEST = 225;
-    /**
-     * The compass direction for west.
-     */
     public static final int WEST = 270;
-    /**
-     * The compass direction for northwest.
-     */
     public static final int NORTHWEST = 315;
-
-    /**
-     * Constructs a location with given row and column coordinates.
-     * @param r the row
-     * @param c the column
-     */
     public Location(int r, int c)
     {
         row = r;
