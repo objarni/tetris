a26642b11f6ec12b073f87832f47639bbe3a7ccb
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2023-04-11 18:20:08 +0200

    Use JUnit reporter explicitly

--- src/test/java/TetrisTest.java
+++ src/test/java/TetrisTest.java
@@ -1,10 +1,13 @@
 import org.approvaltests.Approvals;
+import org.approvaltests.reporters.JunitReporter;
+import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@UseReporter(JunitReporter.class)
 class TetrisTest {
 
     @BeforeEach
