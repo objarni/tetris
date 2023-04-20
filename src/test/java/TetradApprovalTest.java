171e61e1683bfd8a304fb47f339cb85e3847f833
Guillem Hernandez Sola <guillemhernandezsola@Guillems-Mac-mini-M1.local>
2023-04-20 10:23:25 +0200

    Shapes tests

--- src/test/java/TetradApprovalTest.java
+++ src/test/java/TetradApprovalTest.java
@@ -28,4 +28,51 @@ class TetradApprovalTest {
     void doubleRotateBlock() {
     }
 
+    @Test
+    void blockOne() {
+        var grid = new Grid<Block>(10, 10);
+        var tetrad = new Tetrad(grid, 1);
+        Approvals.verify(tetrad);
+    }
+
+    @Test
+    void blockTwo() {
+        var grid = new Grid<Block>(10, 10);
+        var tetrad = new Tetrad(grid, 2);
+        Approvals.verify(tetrad);
+    }
+
+    @Test
+    void blockThree() {
+        var grid = new Grid<Block>(10, 10);
+        var tetrad = new Tetrad(grid, 3);
+        Approvals.verify(tetrad);
+    }
+
+    @Test
+    void blockFour() {
+        var grid = new Grid<Block>(10, 10);
+        var tetrad = new Tetrad(grid, 4);
+        Approvals.verify(tetrad);
+    }
+
+    @Test
+    void blockFive() {
+        var grid = new Grid<Block>(10, 10);
+        var tetrad = new Tetrad(grid, 5);
+        Approvals.verify(tetrad);
+    }
+
+    @Test
+    void blockSix() {
+        var grid = new Grid<Block>(10, 10);
+        var tetrad = new Tetrad(grid, 6);
+        Approvals.verify(tetrad);
+    }
+    @Test
+    void blockSeven() {
+        var grid = new Grid<Block>(10, 10);
+        var tetrad = new Tetrad(grid, 7);
+        Approvals.verify(tetrad);
+    }
 }
